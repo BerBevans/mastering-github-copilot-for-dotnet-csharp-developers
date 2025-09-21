45825bae9269bcdc9463653fc8dc684f67a9e9a4
Evans <ben_r_evans@hotmail.com>
2025-09-21 08:53:49 +0000

    Add Skills1 project and initial implementation of Program.cs

---
+++ code/Skills1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void CalculateNumbers(int a, int b)
+{
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+    double quotient = (double)a / b;
+
+    Console.WriteLine($"Sum: {sum}");
+    Console.WriteLine($"Difference: {difference}");
+    Console.WriteLine($"Product: {product}");
+    Console.WriteLine($"Quotient: {quotient}");
+}
