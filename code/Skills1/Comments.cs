f0f5497acc9c528de8361a3bc5ec795e0c5ccec4
Evans <ben_r_evans@hotmail.com>
2025-09-21 09:01:35 +0000

    Add CommentGenerator class with GenerateComment method for skill comments

---
+++ code/Skills1/Comments.cs
@@ -0,0 +1,9 @@
+namespace Skills1;
+
+public class CommentGenerator
+{
+    public static string GenerateComment(string skill)
+    {
+        return $"This is a comment for the skill: {skill}";
+    }
+}
